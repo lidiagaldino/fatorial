87e2e886e89e44e4d2e8a699f5a2c31a2707a630
LIDIA GABRIELLY DO NASCIMENTO GALDINO <22102372@CFP127ALUNO.LOCAL>
2022-03-30 11:08:37 -0300

    while

--- Fatorial.java
+++ Fatorial.java
@@ -2,13 +2,25 @@ import java.util.Scanner;
 class Fatorial {
     public static void main(String args[]){
         Scanner ler = new Scanner(System.in);
-        System.out.println("Insira um número inteiro: ");
-        int n = ler.nextInt();
-        long mult = 1;
-        for (int i = 2; i <= n ; i++) {
-            mult *= i;
+        int v = 1;
+        while (v == 1) {
+            long mult = 1;
+            System.out.println("Insira um número inteiro positivo: ");
+            int n = ler.nextInt();
+            while (n < 0) {
+                System.out.println(n+" = Número inválido. Digite novamente: ");
+                n = ler.nextInt();
+            }
+            for (int i = 2; i <= n ; i++) {
+                mult *= i;
+            }
+            System.out.println("Fatorial de "+n+": "+mult);
+            System.out.println("Deseja calcular novamente? \n1. Sim\n2. Não");
+            v = ler.nextInt();
+            if (v == 2) {
+                System.out.println("Bye!");
+            }
         }
-        System.out.println("Fatorial de "+n+": "+mult);
         ler.close();
     }
 }
